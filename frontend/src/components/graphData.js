2d605dcb6efb40bccb9a84984de023595488bef6
greataaron <108869192+greataaron@users.noreply.github.com>
2022-07-26 17:20:44 +0200

    Graph & Pagination

--- frontend/src/components/graphData.js
+++ frontend/src/components/graphData.js
@@ -1,48 +1,198 @@
 import Chart from "react-apexcharts";
-import React, { useState, useEffect, useTransition } from 'react';
+import React from 'react';
+import moment from "moment";
 
-const graphData = (data) => {
-    console.log(data)
-    const {cpu,memory, ip, quickLook, sysInfo, upTime, systemCount, localHour, diskIo ,amps, loading, error } = data 
+const CpuGraph = ({data}) => {
+    return(
+        <div>
+            <p>
+                Graphique de l'utilisation du cpu 
+                ({data.cpu.idle ? (100-data.cpu.idle).toFixed(2) : 0}%)
+            </p>
+            <Chart  
+            options =
+            {{
+                labels : ["utilisé"],
+                plotOption : 
+                {
+                    radialBar:
+                    {
+                        startAngle: -90,
+                        endAngle: 90,
+                       track: 
+                       {
+                            background: "#e7e7e7",
+                            strokeWidth: '97%',
+                            margin: 5, // margin is in pixels
+                            dropShadow: 
+                            {
+                              enabled: true,
+                              top: 2,
+                              left: 0,
+                              color: '#999',
+                              opacity: 1,
+                              blur: 2
+                            }
+                        }
+                    }
+                }
+            }}
 
-     /*   const [optionCPU,setOptionCPU] = useState({});
-        const [serieCPU,setSerieCPU] = useState([]);
+                series={[data.cpu.idle ? (100-data.cpu.idle).toFixed(2) : 0]}
+                type="radialBar"
+                // height="50"
+                width="250"
+            />
+            Sur un total de {data.cpu.cpucore ? data.cpu.cpucore : "Non acquis" } coeur logique
+        </div>
+)}
 
-    useEffect(() => {
-        setOptionCPU({
-            Chart: {
-              id: 'cpuGraph',
-              offsetY: -20,
-              sparkline: {
-                enabled: true
-              }
-            }});
-        setSerieCPU(cpu.idle);
-        
-    });
-        */
+const MemGraph = ({data}) => {
 
-    const CpuGraph =
-    <div>
-        <p>Graphique de l'utilisation du cpu ({cpu.idle})</p>
+    return(
+        <div>
+            <p>
+              Utilisation de la Mémoire ({data.memory.total ? (data.memory.total/1e9).toFixed(2) : 0 }Go)  {/*On divise par 1e9 pour ramener le total en Go*/}
+            </p>
+            <Chart  
+            options={{
+                labels : ["Utilisé"],
+                plotOption : 
+                {
+                    startAngle : -90,
+                    endAngle: 90
+                }
+            }}
 
-        <Chart  
-           options={{ labels : (["Disponible"])}}
+                series={[data.memory.percent ? data.memory.percent : 0]}
+                type="radialBar"
+                // height="50"
+                width="250"
+            />
+        </div>
+)}
 
-           plotOption =  {{
-            startAngle: -90,
-            endAngle: 90}}
-
-            series={[cpu.idle]}
-            type="radialBar"
-            // height="50"
-            width="250"
-        />
-        
+const ListDisk = ({data}) => {
+    return(
+        <div>    
+          Liste des disques 
+          {data.diskIo.map 
+          (i => 
+            <li key={i.disk_name}>
+                {i.disk_name}
+            </li>
+          )}
     </div>
 
-    
-    return {CpuGraph};
+    )
 }
 
-export default graphData;
+const ListProcess = ({data}) => {
+    return(
+        <div>    
+          Liste des Applications
+          {data.amps.map 
+          (i => 
+            <li key={i.key}>
+                {i.name}
+            </li>
+          )}
+    </div>
+
+    )
+}
+
+const UpTime = ({data}) => {
+    return (
+      <div>
+        {data.upTime}
+      </div>
+    )
+}
+
+
+
+const LocalHour = ({data}) => {
+    return (
+      <div>
+        {data.localHour}< br/>
+        Format local de la date : {moment.locale()}, 
+        {console.log(moment(data.localHour).format('llll'))} {/**Prend pas la props */}
+        <br/>
+        {data.localHour ? moment(data.localHour).format('llll') : "Error"}
+      </div>
+    )
+}
+
+const GlobalInfo = ({data}) => {
+    return(
+          <div>
+          OS : {data.sysInfo.hr_name} <br/>
+          La machine {data.sysInfo.hostname} est allumé depuis : <br/>
+          <UpTime data={data} />
+          Il est :
+          <LocalHour data={data} /> pour la machine
+        </div>
+    )
+}
+
+const CpuGraphDetail = ({data}) => {  //tentative de graph avec plusieur bar ou plusieur de graph (moins bien)
+    return(
+        <div>
+            <p>
+                Détails de l'utilisation du cpu 
+                ({data.cpu.cpucore ? data.cpu.cpucore : "Non acquis" } coeur)
+            </p>
+            {data.quickLook.percpu ? data.quickLook.percpu.map
+            ( i => 
+              {
+                <Chart  
+                 options =
+                 {{
+                    labels : ["utilisé"],
+                    plotOption : 
+                    {
+                      radialBar:
+                      {
+                        startAngle: -90,
+                        endAngle: 90,
+                      }
+                    }
+                 }}
+                     series={[i.total]}
+                     type="radialBar"
+                     // height="50"
+                     width="250"
+                />
+              }
+            ) : "Not loaded"}
+        </div>
+)}
+
+export {CpuGraph, CpuGraphDetail, MemGraph, ListDisk, ListProcess, UpTime, LocalHour, GlobalInfo, };
+
+
+/**
+ * 
+ * data.quickLook.percpu ? data.quickLook.percpu.map(i => i.total) : 
+ * <Chart  
+ * options =
+ * {{
+ *     labels : [data.quickLook.percpu ? data.quickLook.percpu.map(i => i.cpu_number) : ["0","0","0","0","0","0","0","0"]],
+ *     plotOption : 
+ *     {
+ *         radialBar:
+ *         {
+ *             startAngle: -90,
+ *             endAngle: 90,
+ *         }
+ *     }
+ * }}
+ * 
+ *     series={ data.quickLook.percpu ? data.quickLook.percpu.map(i => i.total) : [0,0,0,0,0,0,0,0]}
+ *     type="radialBar"
+ *     // height="50"
+ *     width="250"
+ * />
+ * 
+*/
--- frontend/src/pages/dashboard/Overview.js
+++ frontend/src/pages/dashboard/Overview.js
@@ -1,22 +1,36 @@
-import {Card} from "hydrogen"
+import {Card, Divider} from "hydrogen"
 import useInstance from "contexts/instance";
 import useData from "hooks/useData";
-import graphData from "components/graphData";
+import {CpuGraph, MemGraph, ListDisk, ListProcess, UpTime, LocalHour, GlobalInfo,  CpuGraphDetail} from "components/graphData";
 
 const Overview = () => {
   const [instance] = useInstance();
   const data = useData(instance.value);
-  const {CpuGraph} = graphData(data); //for later
-
+ // const {cpuGraph} = graphData(data); //for later
+  console.log((data))
   return (
     <div className="">
       <Card>
         <h3 className="">current instance: {instance.label} ({instance.value})</h3>
       </Card>
       <Card>
-        <div>
-          Ceci est une seconde carte
-          {data.cpu.idle}
+        <GlobalInfo data={data} />
+      </Card>
+
+      {/*<Divider className="divide-x-2">*/}
+      <Card>
+          <div style={{display:"flex", gap:20}}>
+            <CpuGraph data={data}/>
+            <MemGraph data={data} />
+            {<CpuGraphDetail data={data} />}
+            {console.log(data.quickLook.percpu ? data.quickLook.percpu.map(i => i.total) : "0")}
+          </div>
+      </Card>
+
+      <Card>
+        <div style={{display:"flex", gap:50}}>
+          <ListDisk data={data}/>
+          <ListProcess data={data}/>
         </div>
       </Card>
     </div>
